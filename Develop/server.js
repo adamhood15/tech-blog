8d757a16e28b8e7aadf75f4f0b9e0a63a2bb9685
Mr. Hood <adamhood15@gmail.com>
2023-05-24 12:55:26 -0500

    Added connectivity to handlesbars pages with some starter data

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = 3001;
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/api/index'));
 
-app.use(express.static('public'));
+  
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`App listening to port ${PORT}`));
+// Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
