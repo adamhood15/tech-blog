466f94f93c78cfd18c4c77d66795f0fbdd61d041
Mr. Hood <adamhood15@gmail.com>
2023-05-24 12:00:51 -0500

    Added code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
 
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`App listening to port ${PORT}`));
