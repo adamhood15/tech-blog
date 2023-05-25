13b5d2e35e8596c9fc83bb59365b36601ef772f2
Mr. Hood <adamhood15@gmail.com>
2023-05-25 11:15:09 -0500

    Added all frontend handlebars pages

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(require('./controllers/api/index'));
 
   
